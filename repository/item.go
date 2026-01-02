b422f6995241b6ee102bbe34e0a77e2617fa80a2
Alif Dwi Rahman <alifdwirahman54@gmail.com>
2026-01-02 17:50:08 +0700

    feat(repository): add item and sale repository implementation

--- repository/item.go
+++ repository/item.go
@@ -1,1 +1,22 @@
 package repository
+
+import (
+	"alfdwirhmn/inventory/database"
+
+	"go.uber.org/zap"
+)
+
+type ItemsRepository interface {
+}
+
+type itemsRepository struct {
+	DB     database.PgxIface
+	Logger *zap.Logger
+}
+
+func NewItemsRepository(db database.PgxIface, log *zap.Logger) ItemsRepository {
+	return &itemsRepository{
+		DB:     db,
+		Logger: log,
+	}
+}
--- repository/sale.go
+++ repository/sale.go
@@ -1,1 +1,22 @@
 package repository
+
+import (
+	"alfdwirhmn/inventory/database"
+
+	"go.uber.org/zap"
+)
+
+type SaleRepository interface {
+}
+
+type saleRepository struct {
+	DB     database.PgxIface
+	Logger *zap.Logger
+}
+
+func NewSaleRepository(db database.PgxIface, log *zap.Logger) SaleRepository {
+	return &saleRepository{
+		DB:     db,
+		Logger: log,
+	}
+}
